515185821791335e14acaf78e2675b79837d0c17
João Vitor Castilho Mattos <joaovitormattos9@gmail.com>
2025-11-08 20:13:16 -0300

    Correção da Tarefa 03 da Aula 13 de Algoritmos e Lógica de Programação.

--- Aula13/aula13tarefa03.py
+++ Aula13/aula13tarefa03.py
@@ -7,9 +7,9 @@ numero_digitado = 1  # Inicializa com um valor diferente de zero para entrar no 
 while numero_digitado != 0:
     numero_digitado = int(input("Digite um número inteiro (0 para encerrar): "))
     if numero_digitado != 0 and numero_digitado % 2 == 0:
-        cont_pares += numero_digitado
+        cont_pares += 1
         print(f"Número {numero_digitado} é par. Continuando..")
     else: #Se for ímpar e diferente de zero
-        cont_impares += numero_digitado
+        cont_impares += 1
         print(f"Número {numero_digitado} é ímpar. Continuando..")
 print(f"Números pares digitados: {cont_pares}. \nNúmeros ímpares digitados: {cont_impares}.")
