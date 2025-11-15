e8d005296b881ddbfbb801f87985901a468d81e1
João Vitor Castilho Mattos <joaovitormattos9@gmail.com>
2025-11-15 19:26:59 -0300

    Exercício simples para estudo de Vetores usando Python.

---
+++ AulaVetores/vetoresatividade01.py
@@ -0,0 +1,19 @@
+# 1 Criação do Vetor(Lista): 
+lista_frutas = ["Maçã", "Banana", "Uva", "Pêra", "Manga"]
+
+print("--- Análise da Lista de Frutas ---")
+
+# 2 Exibição Total: Exibe a lista completa de frutas.
+print("Lista Completa de Frutas:", lista_frutas)
+
+# 3 Acesso por Índice: Primeiro Elemento/Índice 0
+primeiro = lista_frutas[0]
+print(" Primeiro Elemento (Índice 0):", primeiro)
+
+# 4 Acesso por Índice: Terceiro Elemento/Índice 2
+terceiro = lista_frutas[2]
+print(" Terceiro Elemento (Índice 0):", terceiro)
+
+# 5 Acesso por Índice: Último Elemento/Índice -1
+ultimo = lista_frutas[-1]
+print(" Último Elemento (Índice -1):", ultimo)
